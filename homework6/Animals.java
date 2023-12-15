23179dfe701e8fb17cf0010c70ab4459b13af573
Valeria <152556287+Valeria6845@users.noreply.github.com>
2023-12-15 19:50:15 +0200

    Add files via upload

---
+++ homework6/Animals.java
@@ -0,0 +1,15 @@
+package homework6;
+
+public class Animals {
+    private static int amount;
+    public int getAmount() {
+        return amount;
+    }
+    public Animals() {
+        amount++;
+    }
+    public void run(int obstacle_length){
+    }
+    public void swim(int obstacle_length) {
+    }
+}
