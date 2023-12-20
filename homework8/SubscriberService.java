90f0e1d2bf45560e7c86d9b63d7f8f9fadca8cde
Valeria <152556287+Valeria6845@users.noreply.github.com>
2023-12-20 17:07:22 +0200

    Delete homework8 directory

--- homework8/SubscriberService.java
+++
@@ -1,9 +0,0 @@
-package homework8;
-
-public interface SubscriberService {
-    public void showCityCallDurationExceedTheLimit(Subscriber[] list);
-    public void showInterCityCallDuration(Subscriber[] list);
-    public void showInformationByTheFirstLetter(Subscriber[] list, String firstLetter);
-    public int calculateSumOfInternetTraffic(Subscriber[] list, String city);
-    public int calculateSubsWithNegativeBalance(Subscriber[] list);
-}
