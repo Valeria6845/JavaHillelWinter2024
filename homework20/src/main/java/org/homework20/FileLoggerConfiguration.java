3030c80af684df4f2b5f495169f2954655b9649a
Valeria <152556287+Valeria6845@users.noreply.github.com>
2024-02-16 20:07:27 +0200

    Add files via upload

---
+++ homework20/src/main/java/org/homework20/FileLoggerConfiguration.java
@@ -0,0 +1,33 @@
+package org.homework20;
+
+import lombok.Getter;
+
+import java.io.File;
+import java.io.IOException;
+@Getter
+
+public class FileLoggerConfiguration {
+    private File file;
+    private String path;
+    private LoggingLevel logLvl;
+    private int maxSize;
+    private int counter;
+    private String format;
+    public FileLoggerConfiguration(String path, LoggingLevel logLvl, int maxSize, String format) throws IOException {
+        this.format = format;
+        counter++;
+        this.file = new File(path+"\\log_file"+counter);
+        file.createNewFile();
+        this.path = path+"\\log_file"+counter;
+        this.logLvl = logLvl;
+        this.maxSize = maxSize;
+
+    }
+    public void levelChange() {
+        if (logLvl.equals(LoggingLevel.DEBUG)) {
+            logLvl = LoggingLevel.INFO;
+        }else {
+            logLvl = LoggingLevel.DEBUG;
+        }
+    }
+}
