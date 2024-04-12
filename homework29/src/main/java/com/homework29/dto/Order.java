63aca8c8abb6238c08fb1ea4318ab5ad8e9e95e3
Valeria <152556287+Valeria6845@users.noreply.github.com>
2024-04-12 23:33:11 +0300

    Add files via upload

---
+++ homework29/src/main/java/com/homework29/dto/Order.java
@@ -0,0 +1,22 @@
+package com.homework29.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+public class Order {
+    private int id;
+    private LocalDate date;
+    private int cost;
+    private List<Product> products;
+}
